b7c1976ba265d3c169babbc3d354903a11441d44
CantarinoG <guilhermecantarino1709@gmail.com>
2024-08-15 08:26:49 -0300

    feat: Add binary search algorithm.

---
+++ searching/binary_search.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int target)
+{
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        else if (arr[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2, 4, 8, 10, 12, 14, 16, 18, 20}; // Array must be sorted.
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 0;
+
+    cout << "Which element(int) are you looking for?" << endl;
+    cin >> target;
+
+    int result = binarySearch(arr, size, target);
+
+    if (result == -1)
+    {
+        cout << "Element not found." << endl;
+    }
+    else
+    {
+        cout << "Element found at index " << result << "." << endl;
+    }
+
+    return 0;
+}
